ab7c164c4c3e8c8931673c53c7e75fa5abf92e6c
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-04-24 21:13:49 -0500

    added pics

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+$('a').click(function(){
+    if (this.text == 'Home') {
+        window.location.href = 'index.html';
+    } else if (this.text == 'About') {
+        window.location.href = 'about.html';
+    } else if (this.text == 'Portfolio') {
+        window.location.href = 'portfolio.html';
+    }
+})
+
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('body').style.display = "";},100);
+    }
